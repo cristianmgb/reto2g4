59bd56fd77e28db376245287b750269acdf0e664
Cristian Gonzalez <cristianmgb21@gmail.com>
2023-07-27 20:26:52 -0500

    reto 2 - initial commit

---
+++ src/main/java/com/desaextremo/retotres/RetotresApplication.java
@@ -0,0 +1,18 @@
+package com.desaextremo.retotres;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetotresApplication implements CommandLineRunner {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetotresApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	}
+}
